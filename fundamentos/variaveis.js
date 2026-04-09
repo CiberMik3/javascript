0a7201148b04c4249d1c429adde2a653749aa029
CiberMik3 <mikedeoliveira2022@gmail.com>
2026-04-08 22:23:21 -0300

    fiz as variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,45 @@
+/**
+ * Estudo das variaveis
+ * @author Mike de oliveira
+ */
+
+console.clear() /** LIMPAR TELA */
+console.log("Estudo das variaveis")
+console.log("") /** PULAR LINHA */
+console.log("Variavel do tipo string")
+/**
+ * A LINHA ABAIXO CRIA UMA VARIAVEL
+ */
+let nome = "MIke de oliveira"
+console.log (nome)
+console.log(typeof(nome)) /** TIPO DE VARIAVEL */
+console.log("")
+console.log("Variavel do tipo number")
+let idade = 20
+console.log(idade)
+console.log(typeof(idade))
+let peso = 80
+console.log(peso)
+console.log(typeof(peso))
+let altura = 1.70
+console.log(altura)
+console.log(typeof(altura))
+console.log("")
+console.log("Variavel do tipo boolean")
+let vip = true
+console.log(vip)
+console.log(typeof(vip))
+
+console.log("")
+console.log("------------------------")
+console.log("-----Ficha do aluno-----")
+console.log("------------------------")
+/** A LINHA A BAIXO CONCATENA (UNIAO) UM TEXTO COM O CONTEUDO DA VARIAVEL */
+console.log(`Nome: ${nome}`) /**  unir um testo com uma variavel */
+console.log(`Idade: ${idade}`)
+console.log(`peso: ${peso}kg`)
+console.log(`altura: ${altura}`)
+console.log(`vip: ${vip}`)
+console.log("------------------------")
+
+
