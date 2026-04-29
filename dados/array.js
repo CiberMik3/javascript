c426168ce19b59aaf0b899efe80585687ba80c41
CiberMik3 <mikedeoliveira2022@gmail.com>
2026-04-28 22:33:07 -0300

    cartas com vetor

---
+++ dados/array.js
@@ -0,0 +1,17 @@
+/**
+ * Estrutura de dados - Array(vetor)
+ * @author Mike de oliveira
+ */
+
+console.clear()
+
+let alunos = ["Vitor" ,"Tania", "Pedro", "Maria", "Vivi", "Ana"]
+console.log(typeof(alunos))
+
+// Exebindo dados de um array
+console.log(alunos)
+console.table(alunos)
+console.log(alunos[2])
+
+// Obtendo o tamanho do Array
+console.log(alunos.length)
