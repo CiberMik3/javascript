4712e2bf5eca33c0a5136b18fda95e5cd537b877
CiberMik3 <mikedeoliveira2022@gmail.com>
2026-05-12 22:28:21 -0300

    Sistema para gestão de academia e Estudo de array com estrutura de dados

--- dados/academia.js
+++ dados/academia.js
@@ -66,7 +66,7 @@ function mainAcademia() {
                 gerarRelatorios()
                 break;
             case 0:
-               console.log("Ecerrando sistema.")
+                console.log("Ecerrando sistema.")
                 break;
             default:
                 console.log("Opção invalida")
@@ -83,6 +83,35 @@ function cadrastarAluno() {
     console.clear()
     console.log("=== CADASTRO DE ALUNOS ===".underline)
     console.log()
+
+    // Lógica principal
+    nome = prompt("Nome: ")
+    idade = Number(prompt("Idade: "))
+    peso = Number(prompt("Peso: "))
+    altura = Number(prompt("Altura: "))
+    vip = prompt("Aluno VIP (s/n)?: ")
+    if (vip === "s") {
+        vip = true
+    } else {
+        vip = false
+    }
+
+    // Adicionar os dados na matriz
+    alunos.push([
+        matricula,
+        nome,
+        idade,
+        peso,
+        altura,
+        vip
+    ])
+
+    matricula++ // Auto incremento da matricula
+
+    console.log()
+    console.log("Aluno(a) cadastrado(a) com sucesso")
+
+
     prompt("ENTER...")
 
 }
@@ -93,11 +122,85 @@ function cadrastarAluno() {
 // CRUD - Read >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 function consultarAlunos() {
 
+    // Sub Menu
+    do {
+        console.clear()
+        console.log("=== CONSULTA DE ALUNOS ===".underline)
+        console.log()
+        console.log("1. Buscar aluno")
+        console.log("2. Listar alunos")
+        console.log("0. Voltar")
+
+        opcaoConsulta = Number(prompt("Escolha: "))
+
+
+        switch (opcaoConsulta) {
+            case 1:
+                buscarAluno()
+                break;
+            case 2:
+                listarAlunos()
+                break
+            case 0:
+                break
+            default:
+                console.log("")
+                console.log("Opção invalida")
+                prompt("Enter...")
+        }
+
+    } while (opcaoConsulta !== 0);
+
+    // Buscar aluno
+    function buscarAluno() {
+        console.clear()
+        console.log("=== BUSCAR ALUNO ===")
+        console.log()
+
+        Prompt("Enter...")
+    }
+
+    function listarAlunos() {
+        console.clear()
+        console.log("=== LISTA DE ALUNOS ===")
+        console.log()
+
+        // Validação (se nenhum alunos cadastrado)
+        //// lengh = tamanho do vetor
+        if (alunos.length === 0) {
+            console.log("Nenhum aluno cadastrado".bgRed)
+        } else {
+            // console.table(alunos)
+            // Ordenar os nomes (criar copia do array)
+            let alunosOrdenados = [...alunos]
+            alunosOrdenados.sort((a, z) => {
+                return a[1].localeCompare(z[1])
+            })
+
+            // Criando um cabeçario para a tabela
+            //console.table(alunosOrdenados)
+            let listaAlunos = alunosOrdenados
+            .map((a) => {
+                return {
+                    Matrícula: a[0],
+                    Nome: a[1],
+                    idade: a[2],
+                    peso: a[3],
+                    altura: a[4],
+                    VIP: a[5]
+                }
+            })
+            console.table(listaAlunos)
+        }
+
+        prompt("Enter...")
+    }
 }
 // CRUD - Read ( Fim ) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 // CRUD - Update >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 function editarAluno() {
     console.clear()
     console.log("=== ALTERAR ALUNO ===".underline)
@@ -129,10 +232,64 @@ function gerarFichaAluno() {
 
 // Relatorios >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 function gerarRelatorios() {
-    console.clear()
-    console.log("=== RELATORIOS ===".underline)
-    console.log()
-    prompt("ENTER...")
+    do {
+        //Sub Menu
+        console.clear()
+        console.log("=== RELATÓRIOS ===".underline)
+        console.log()
+        console.log("1. Alunos VIP ")
+        console.log("2. Média de idade")
+        console.log("3. % IMC dos alunos")
+        console.log("0. Voltar")
+
+        opcaoRelatorio = Number(prompt("Escolha: "))
+
+
+        switch (opcaoRelatorio) {
+            case 1:
+                gerarRelatorioVip()
+                break;
+            case 2:
+                gerarRelatorioMediaIdade()
+                break
+            case 3:
+                gerarRelatorioMediaIdade()
+                break
+            case 0:
+                gerarRelatorioImc()
+                break
+            default:
+                console.log("")
+                console.log("Opção invalida")
+                prompt("Enter...")
+        }
+
+    } while (opcaoRelatorio !== 0);
+
+    // Relatorio de alunos vip
+    function gerarRelatorioVip() {
+        console.clear()
+        console.log("=== ALUNOS VIP ===".underline)
+        console.log()
+
+        prompt("ENTER...")
+    }
+    // Relatorio média de aidade dos alunos
+    function gerarRelatorioMediaIdade() {
+        console.clear()
+        console.log("=== MEDIA DE IDADE ===".underline)
+        console.log()
+
+        prompt("ENTER...")
+    }
+    // Relatorio de percentural de IMC
+    function gerarRelatorioImc() {
+        console.clear()
+        console.log("=== IMC DOS ALUNOS ===".underline)
+        console.log()
+
+        prompt("ENTER...")
+    }
 }
 // Relatorios ( Fim ) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
